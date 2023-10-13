a56d2f780661003ca7d7cf4756abd286bc69594d
AngelLaines <angelmoreno755@gmail.com>
2023-10-13 15:33:20 -0700

    first commit

---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+    <form>
+        <div class="campo">
+            <label for="">Definir presupuesto</label>
+            <input 
+                type="text"
+                id="nuevo-presupuesto"
+                class="nuevo-presupuesto"
+                placeholder="Añade tu presupuesto" 
+            />
+        </div>
+        <input 
+            type="submit" 
+            value="Definir Presupuesto"
+            
+        />
+    </form>
+</template>
+
+<style  scoped>
+
+</style>
